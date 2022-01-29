542957d59e5704bdce276cd1269ceb02c237cb34
Astrid20220 <yadiragonzalez556@gmail.com>
2022-01-28 23:42:54 -0600

    browser

--- src/index.js
+++ src/index.js
@@ -12,16 +12,28 @@
  .then((responseJson) => {
      responseJson.data.array.forEach((item) => {
  
-         const imagen = document.createElement("img");
-        
- 
-         const title = document.createElement("h2");
-         document.body.appendChild(title);
- 
-         const price = document.createElement("div");
-         document.body.appendChild(price);
-         
-         console.log(item.name);  
-     });
-  
- });
+        const imagen = document.createElement("img");
+        //url imagen
+        //imagen.className =
+           // "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+        imagen.src = `${baseUrl}${item.image}`;
+        //crear titulo
+        const title = document.createElement("h2");
+        title.className = 'text-xl text-blue-900';
+        title.textContent = item.name;
+
+        //crear precio
+        const price = document.createElement("div");
+        price.textContent = formatPrice(item.price);
+
+        const taste = document.createElement("p");
+        taste.textContent = item.sku;
+
+        const container = document.createElement("div");
+        container.append(imagen, title, price, taste);
+
+        todosLosItems.push(container);
+    });
+
+    appNode.append(...todosLosItems);
+})
